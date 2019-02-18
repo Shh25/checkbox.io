842c00938f86a6b13d97854488100d802efa3582
Shefali Agarwal <sdagarwa@ncsu.edu>
2019-02-17 19:05:40 -0500

    Server: Added start, stop in server file for test

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -85,6 +85,41 @@ app.post('/api/study/admin/notify/', admin.notifyParticipant);
 
 
 
-var port = process.env.APP_PORT;
-app.listen(port);
-console.log(`Listening on port ${port}...`);
+var appPort = process.env.APP_PORT;
+// app.listen(port);
+// console.log(`Listening on port ${port}...`);
+
+function start() 
+{
+	return new Promise(function(resolve,reject)
+	{
+		server = app.listen(appPort, function () {
+
+			var host = server.address().address
+			var port = server.address().port
+
+			console.log('Example app listening at http://%s:%s', host, port)
+			resolve({host: host, port: port});
+		}).on('error', function (err) {
+			if(err.errno === 'EADDRINUSE') {
+				console.log(`----- Port ${port} is busy, try with another port`);
+			} else {
+				console.log(err);
+			}
+		});
+	});
+}
+
+function stop() 
+{
+	return server.close();
+}
+
+// (async () => {
+// 	if( CMD === "start" )
+// 	{
+// 		await start();
+// 	}
+// })();
+
+module.exports = { start: start, stop: stop };
