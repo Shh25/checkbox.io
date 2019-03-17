c3048ab4b875e6b4412d13cd7bcc88826db15ae2
Prayani <prayani.singh0106@gmail.com>
2019-03-16 20:38:59 -0400

    new file paths

--- server-side/site/fileNameList.js
+++ server-side/site/fileNameList.js
@@ -2,8 +2,15 @@ var n = require("./analysis");
 
 var admin = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/admin.js";
 var create = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/create.js"
+var csv = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/csv.js";
+var designer = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/designer.js";
+var live = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/live.js";
+var study = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/study.js";
+var studyModel = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/studyModel.js";
+var upload = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/upload.js";
 
-var fileNameArr = [admin,create];
+var fileNameArr = [admin,create,csv,designer,live,study,studyModel,upload];
+
 console.log(`shds ${fileNameArr[0]}`)
 function callAnalysis()
 {
