337302cfd2b29c9d09b97594114560690a63df1f
Prayani <prayani.singh0106@gmail.com>
2019-03-17 21:23:30 -0400

    relative paths

--- server-side/site/fileNameList.js
+++ server-side/site/fileNameList.js
@@ -1,14 +1,16 @@
 var n = require("./analysis");
 
-var admin = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/admin.js";
-var create = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/create.js"
-var csv = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/csv.js";
-var designer = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/designer.js";
-var live = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/live.js";
-var study = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/study.js";
-var studyModel = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/studyModel.js";
-var upload = "/Users/prayanisingh/Documents/DevOpsProj/CheckBox/checkbox.io/server-side/site/routes/upload.js";
-var fileNameArr = [admin,create,csv,designer,live,study,studyModel,upload];
+var admin = "routes/admin.js";
+var create = "routes/create.js"
+var csv = "routes/csv.js";
+var designer = "routes/designer.js";
+var live = "routes/live.js";
+var study = "routes/study.js";
+var studyModel = "routes/studyModel.js";
+var upload = "routes/upload.js";
+var testing = "routes/testing.js";
+
+var fileNameArr = [testing, admin,create,csv,designer,live,study,studyModel,upload];
 
 function callAnalysis()
 {
